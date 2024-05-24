c40e426eedd3604e59925d3713269a85cdd19228
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-05-24 15:00:16 +0530

    supplier company

--- src/main/java/com/lifePill/SupplierService/controller/SupplierCompanyController.java
+++ src/main/java/com/lifePill/SupplierService/controller/SupplierCompanyController.java
@@ -3,6 +3,7 @@ package com.lifePill.SupplierService.controller;
 import com.lifePill.SupplierService.dto.SupplierCompanyDTO;
 import com.lifePill.SupplierService.service.SupplierCompanyService;
 import com.lifePill.SupplierService.util.StandardResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
@@ -11,50 +12,80 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+/**
+ * Controller class for managing supplier company-related endpoints.
+ */
 @RestController
-@RequestMapping("lifepill/v1/supplier-company")
+@RequestMapping("/lifepill/v1/supplierCompanies")
+@AllArgsConstructor
 public class SupplierCompanyController {
 
-    @Autowired
     private SupplierCompanyService supplierCompanyService;
 
-    @PostMapping("/saved")
-    public ResponseEntity<StandardResponse> saveSupplierCompany(@RequestBody SupplierCompanyDTO supplierCompanyDTO){
-        SupplierCompanyDTO saveSupplierCompany = supplierCompanyService.saveSupplierCompany(supplierCompanyDTO);
-        return new ResponseEntity<StandardResponse>(
-                new StandardResponse(201,  "Supplier Saved Successfully",saveSupplierCompany),
-                HttpStatus.CREATED);
-    }
-    @GetMapping("get-supplier-Company-By-Id/{supplierCompanyId}")
-    public ResponseEntity<StandardResponse> getSupplierCompanyById(@PathVariable(value ="supplierCompanyId") Long supplierCompanyId){
-        SupplierCompanyDTO supplierCompanyDTO = supplierCompanyService.getSupplierByCode(supplierCompanyId);
-
-        return new ResponseEntity<StandardResponse>(
-                new StandardResponse(201,supplierCompanyId + " Supplier company get Successfully",supplierCompanyDTO),
-                HttpStatus.CREATED);
+    /**
+     * Retrieves all supplier companies.
+     *
+     * @return ResponseEntity containing a list of all supplier companies.
+     */
+    @GetMapping(path = "/getAll-Supplier-Companies")
+    public ResponseEntity<List<SupplierCompanyDTO>> getAllSupplierCompanies() {
+        List<SupplierCompanyDTO> companies = supplierCompanyService.getAllSupplierCompanies();
+        return new ResponseEntity<>(companies, HttpStatus.OK);
     }
 
-    @GetMapping(path="/get-all-supplierCompany")
-    public ResponseEntity<StandardResponse> getAllSupplierCompany(){
-        List<SupplierCompanyDTO> allSupplierCompany = supplierCompanyService.getAllSupplierCompany();
-        return new ResponseEntity<StandardResponse>(
-                new StandardResponse(201,"SUCCESS", allSupplierCompany),
-                HttpStatus.OK
-        );
+    /**
+     * Saves a new supplier company.
+     *
+     * @param supplierCompanyDTO The SupplierCompanyDTO object containing information about the new supplier company.
+     * @return ResponseEntity containing the saved supplier company.
+     */
+    @PostMapping(path = "/save")
+    public ResponseEntity<SupplierCompanyDTO> saveSupplierCompany(
+            @RequestBody SupplierCompanyDTO supplierCompanyDTO
+    ) {
+        SupplierCompanyDTO savedCompany = supplierCompanyService
+                .saveSupplierCompany(supplierCompanyDTO);
+        return new ResponseEntity<>(savedCompany, HttpStatus.CREATED);
     }
 
-    @DeleteMapping(path = "/delete-supplierCompany/{id}")
-    public ResponseEntity<StandardResponse> deleteSupplierCompany(@PathVariable(value = "id") long supplierCompanyId){
-        String deleted = supplierCompanyService.deleteSupplierCompany(supplierCompanyId);
-        return new ResponseEntity<StandardResponse>(
-                new StandardResponse(201,"DELETED", deleted),
-                HttpStatus.OK
-        );
-    }
-    @PutMapping("/update")
-    public String updateSupplierCompany(@RequestBody SupplierCompanyDTO supplierCompanyDTO){
-        String message = supplierCompanyService.updateSupplierCompany(supplierCompanyDTO);
-        return message;
+    /**
+     * Updates a supplier company by its ID.
+     *
+     * @param id               The ID of the supplier company to update.
+     * @param updatedCompanyDTO The updated supplier company DTO.
+     * @return ResponseEntity containing the updated supplier company DTO and HTTP status.
+     */
+    @PutMapping(path = "/update-supplier-company/{id}")
+    public ResponseEntity<SupplierCompanyDTO> updateSupplierCompanyById(
+            @PathVariable("id") long id, @RequestBody SupplierCompanyDTO updatedCompanyDTO
+    ) {
+        SupplierCompanyDTO updatedCompany =
+                supplierCompanyService.updateSupplierCompanyById(id, updatedCompanyDTO);
+        return new ResponseEntity<>(updatedCompany, HttpStatus.OK);
     }
 
+    /**
+     * Deletes a supplier company by its ID.
+     *
+     * @param id The ID of the supplier company to delete.
+     * @return ResponseEntity with HTTP status indicating the success of the deletion.
+     */
+    @DeleteMapping(path = "/delete-supplier-company/{id}")
+    public ResponseEntity<Void> deleteSupplierCompanyById(@PathVariable("id") long id) {
+        supplierCompanyService.deleteSupplierCompanyById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    /**
+     * Retrieves a supplier company by its ID.
+     *
+     * @param id The ID of the supplier company to retrieve.
+     * @return ResponseEntity containing the retrieved supplier company DTO.
+     */
+    @GetMapping(path ="/get-supplier-company/{id}")
+    public ResponseEntity<SupplierCompanyDTO> getSupplierCompanyById(@PathVariable("id") long id) {
+        SupplierCompanyDTO companyDTO = supplierCompanyService.getSupplierCompanyById(id);
+        return new ResponseEntity<>(companyDTO, HttpStatus.OK);
+    }
 }
