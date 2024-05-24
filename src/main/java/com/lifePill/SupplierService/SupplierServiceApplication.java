5c7fd5431bb3c63fa7935a4b56a9a2647f93552d
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-05-25 04:18:02 +0530

    updated

--- src/main/java/com/lifePill/SupplierService/SupplierServiceApplication.java
+++ src/main/java/com/lifePill/SupplierService/SupplierServiceApplication.java
@@ -30,6 +30,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 		)
 )
 @SpringBootApplication
+//@EnableEurekaClient
 public class SupplierServiceApplication {
 
 	public static void main(String[] args) {
